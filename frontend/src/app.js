20447ecb202393e553165ab8e48f09d0c5dd6eb0
Serhat Ozturk <so2turk@yahoo.com>
2022-05-15 22:48:02 +0200

    Get vids from backend

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './app.css'
 
 function App() {
+	const [videos, setVideos] = useState([])
+
+	useEffect(() => {
+		getVideos()
+
+		// eslint-disable-next-line
+	}, [])
+
+	const getVideos = async () => {
+		try {
+			const result = await axios.get('/pexels/vids')
+			setVideos(result.data.videos)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+	console.log(videos)
+
 	return <div className="App">lol</div>
 }
 
