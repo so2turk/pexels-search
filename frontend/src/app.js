1c77d303083b6388d75118dd3c1376e4968d720f
Serhat Ozturk <so2turk@yahoo.com>
2022-05-15 23:00:09 +0200

    Diplay videos with the vidcard component

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './app.css'
+import VidCard from './components/card/vid-card'
 
 function App() {
 	const [videos, setVideos] = useState([])
@@ -21,7 +22,18 @@ function App() {
 	}
 	console.log(videos)
 
-	return <div className="App">lol</div>
+	return (
+		<div className="App">
+			{videos.length > 0 &&
+				videos.map((video, i) => {
+					return (
+						<div key={`${video.id} + ${i}`}>
+							<VidCard video={video} />
+						</div>
+					)
+				})}
+		</div>
+	)
 }
 
 export default App
