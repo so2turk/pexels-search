d07201a7b69fde36f25e010f7f4b652857b370e6
Serhat Ozturk <so2turk@yahoo.com>
2022-05-18 16:46:28 +0200

    Create a spinner for loading

---
+++ frontend/src/components/spinner/spinner.jsx
@@ -0,0 +1,15 @@
+import './spinner.css'
+
+const Spinner = () => {
+	return (
+		<div className="view-box">
+			<div className="spinner1">
+				<div className="spinner2">
+					<div className="spinner3"></div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Spinner
