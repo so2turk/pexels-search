b05e1704929e7562a382e69072afb56d2ec1b08b
Serhat Ozturk <so2turk@yahoo.com>
2022-05-19 11:29:18 +0200

    Create videos page and clean app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,68 +1,10 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import VideosPage from './pages/videos-page/videos-page'
 import './app.css'
-import VidCard from './components/card/vid-card'
-import Spinner from './components/spinner/spinner'
 
 function App() {
-	const [videos, setVideos] = useState([])
-	const [totalVids, setTotalVids] = useState(0)
-	const [loading, setLoading] = useState(false)
-	let page = 1
-
-	useEffect(() => {
-		if (!videos.length > 0) getVideos()
-
-		window.addEventListener('scroll', handleScroll)
-		// eslint-disable-next-line
-	}, [])
-
-	const getVideos = async () => {
-		try {
-			const result = await axios.get('/api/pexels/vids', { page })
-
-			if (page > 1) {
-				setVideos((videos) => [...videos, ...result.data.videos])
-			} else {
-				setTotalVids(result.data.total_results)
-				setVideos(result.data.videos)
-			}
-		} catch (err) {
-			console.log(err)
-		}
-		setLoading(false)
-	}
-
-	const handleScroll = () => {
-		if (
-			window.innerHeight + document.documentElement.scrollTop ===
-			document.documentElement.offsetHeight
-		) {
-			setLoading(true)
-			page += 1
-			getVideos()
-		}
-	}
-
 	return (
 		<div className="app">
-			{videos.length > 0 && (
-				<div className="total-vids">Totol Videos: {totalVids}</div>
-			)}
-			<div className="cards">
-				{videos.length > 0 ? (
-					videos.map((video, i) => {
-						return (
-							<div className="cardContainer" key={`${video.id} + ${i}`}>
-								<VidCard video={video} />
-							</div>
-						)
-					})
-				) : (
-					<Spinner />
-				)}
-			</div>
-			{loading && <Spinner />}
+			<VideosPage />
 		</div>
 	)
 }
