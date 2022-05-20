3ff921f0727af1db4e474f2111867e1e6cba3fcd
Serhat Ozturk <so2turk@yahoo.com>
2022-05-20 22:03:10 +0200

    Install bcryptjs and create register controller

--- backend/src/controllers/users-controller.js
+++ backend/src/controllers/users-controller.js
@@ -1,4 +1,31 @@
-export const createUser = async (req, res) => {}
+import User from '../models/user.js'
+import bcrypt from 'bcryptjs'
+
+const saltRounds = 11
+
+export const createUser = async (req, res) => {
+	const { name, email, password } = req.body
+
+	try {
+		if (!name || !email || !password)
+			return res.status(400).json('Please add all fields')
+
+		const userExists = await User.findOne({ email: email })
+		if (userExists) return res.status(400).json('User already exists')
+
+		const salt = await bcrypt.genSalt(saltRounds)
+		const hashedPassword = await bcrypt.hash(password, salt)
+
+		const newUser = await User.create({
+			email,
+			name,
+			password: hashedPassword,
+		})
+		res.status(200).json({ success: `New user ${newUser.name} created` })
+	} catch (err) {
+		res.status(500).json({ msg: 'Something went wrong', eMsg: err.message })
+	}
+}
 
 export const login = async (req, res) => {}
 
