a0ac0a7cbf9e31c57e9af9679df74f379eca5ad5
Serhat Ozturk <so2turk@yahoo.com>
2022-05-21 13:09:31 +0200

    Enable register functionality

--- frontend/src/pages/register/register.jsx
+++ frontend/src/pages/register/register.jsx
@@ -1,7 +1,50 @@
+import axios from 'axios'
+import { useRef, useState } from 'react'
 import './register.css'
 
-const Register = () => {
-	return <>Register</>
+const Register = ({ setShowReg }) => {
+	const [regSuccess, setRegSucces] = useState(false)
+	const [regFailure, setRegFailure] = useState(false)
+	const emailRef = useRef()
+	const nameRef = useRef()
+	const passwordRef = useRef()
+
+	const handleReg = async (e) => {
+		e.preventDefault()
+
+		const userToCreate = {
+			email: emailRef.current.value,
+			name: nameRef.current.value,
+			password: passwordRef.current.value,
+		}
+
+		try {
+			await axios.post('/api/users/register', userToCreate)
+			setRegSucces(true)
+			setRegFailure(false)
+		} catch (err) {
+			console.log(err)
+			setRegFailure(true)
+			setRegSucces(false)
+		}
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleReg}>
+				<input type="email" placeholder="E-mail" ref={emailRef} />
+				<input type="text" placeholder="User Name" ref={nameRef} />
+				<input type="password" placeholder="Password" ref={passwordRef} />
+				<button type="submit">Register</button>
+			</form>
+			<div>
+				{regSuccess && (
+					<span className="success">Registration is successful</span>
+				)}
+				{regFailure && <span className="failure">Registration is failed</span>}
+			</div>
+		</div>
+	)
 }
 
 export default Register
