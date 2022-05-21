4aac8539e6116cbd209af7bc27e1f365794edc0e
Serhat Ozturk <so2turk@yahoo.com>
2022-05-21 15:24:02 +0200

    Implement css to navbar

--- frontend/src/components/nav/nav.jsx
+++ frontend/src/components/nav/nav.jsx
@@ -1,7 +1,44 @@
+import { Link } from 'react-router-dom'
 import './nav.css'
 
 const Nav = ({ user }) => {
-	return <>Nav</>
+	return (
+		<div className="nav-body">
+			<div className="nav">
+				<Link to="/">
+					<img className="logo-img" src="/Logo.svg" alt="logo" />
+				</Link>
+				<div className="wrap">
+					<form className="search">
+						<input
+							type="text"
+							className="input-field"
+							placeholder="Search videos..."
+						/>
+						<button type="submit" className="bSubmit button">
+							Search
+						</button>
+					</form>
+				</div>
+				{user ? (
+					<div className="avatar-field">
+						<button className="bLogout button">Logout</button>
+					</div>
+				) : (
+					<>
+						<div className="log-res">
+							<Link to="/login">
+								<button className="bLogin button">Login</button>
+							</Link>
+							<Link to="/register">
+								<button className="bRegister button">Register</button>
+							</Link>
+						</div>
+					</>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default Nav
